d39d1ce3789ca9b615be710796ff5bf50d7391f8
Emerentius <emerentius@arcor.de>
2018-07-26 19:28:49 +0200

    initial commit
    code to create wasm library

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(use_extern_macros)]
+
+#[macro_use]
+extern crate stdweb;
+extern crate sudoku;
+
+use stdweb::js_export;
+use sudoku::Sudoku;
+
+#[js_export]
+fn solve(string: &str) -> Vec<Vec<u8>> {
+    let sudoku = Sudoku::from_str_line(string);
+
+    sudoku.ok()
+        .map_or(vec![], |sud| sud.solve_at_most(2))
+        .into_iter()
+        .map(|solution|
+            solution.to_bytes().to_vec()
+        )
+        .collect::<Vec<_>>()
+}
