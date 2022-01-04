d82682791fd14a816ce886b461275d837a563754
Martin-Louis Bright <mlbright@gmail.com>
2022-01-03 22:53:27 -0500

    Mutable output

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(use_extern_macros)]
-
 #[macro_use]
 extern crate stdweb;
 extern crate sudoku;
@@ -18,9 +16,9 @@ fn solve(grid: &str) -> Vec<Vec<u8>> {
 
     let solution = solution.unwrap();
 
-    let output: Vec<Vec<u8>> = vec![];
+    let mut output: Vec<Vec<u8>> = vec![];
     for i in 0..9 {
-        let intermediate: Vec<u8> = vec![];
+        let mut intermediate: Vec<u8> = vec![];
         for j in 0..9 {
             let digit = solution[(i * 9) + j].first().unwrap() as u8;
             intermediate.push(digit);
