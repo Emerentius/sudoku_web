898ec4a4b67010a4b5e41cb48d1f4ee0abe232dc
Martin-Louis Bright <mlbright@gmail.com>
2022-01-07 00:40:46 -0500

    remove logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate stdweb;
 extern crate sudoku;
-extern crate web_sys;
-extern crate wasm_bindgen;
+// extern crate web_sys;
+// extern crate wasm_bindgen;
 
 use stdweb::js_export;
 use sudoku::Sudoku;
@@ -11,18 +11,18 @@ use sudoku::Sudoku;
 fn solve(grid: &str) -> Vec<Vec<u8>> {
     let sudoku = Sudoku::new();
     let mut solutions: Vec<Vec<u8>> = vec![];
-    web_sys::console::log_1(&"Before solving...".into());
+    // web_sys::console::log_1(&"Before solving...".into());
 
 
     let solution = sudoku.solve(grid);
-    web_sys::console::log_1(&"After solving...".into());
+    // web_sys::console::log_1(&"After solving...".into());
     if !solution.is_none() {
-        web_sys::console::log_1(&"Solution not none!".into());
+        // web_sys::console::log_1(&"Solution not none!".into());
         let solution = solution.unwrap();
-        web_sys::console::log_1(&"Solution unwrapped!".into());
+        // web_sys::console::log_1(&"Solution unwrapped!".into());
 
         solutions.push(sudoku.format_grid(&solution).as_bytes().to_vec());
-        web_sys::console::log_1(&"Solution converted and pushed!".into());
+        // web_sys::console::log_1(&"Solution converted and pushed!".into());
     }
     solutions
 }
