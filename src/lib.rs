58da0dd3a4fa847e427a624e5a41b71838f34ac0
Martin-Louis Bright <mlbright@gmail.com>
2022-01-06 22:14:53 -0500

    console

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 extern crate stdweb;
 extern crate sudoku;
 
+use wasm_bindgen::prelude::*;
 use stdweb::js_export;
 use sudoku::Sudoku;
+use web_sys::console;
 
 #[js_export]
 fn solve(grid: &str) -> Vec<Vec<u8>> {
     let sudoku = Sudoku::new();
     let mut solutions: Vec<Vec<u8>> = vec![];
+    web_sys::console::log_1(&"Before solving...".into());
+
 
     let solution = sudoku.solve(grid);
     if !solution.is_none() {
