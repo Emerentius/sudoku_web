bfd7beacfeb3cb6c11cf30a513b8a635b55f527d
Martin-Louis Bright <mlbright@gmail.com>
2022-01-07 23:58:29 -0500

    wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,16 @@
-#[macro_use]
-extern crate stdweb;
-extern crate sudoku;
-// extern crate web_sys;
-// extern crate wasm_bindgen;
-extern crate console_error_panic_hook;
-
-use stdweb::js_export;
+use wasm_bindgen::prelude::*;
 use sudoku::Sudoku;
 
-#[js_export]
-fn solve(grid: &str) -> Vec<Vec<u8>> {
-    console_error_panic_hook::set_once();
+#[wasm_bindgen]
+pub fn solve(grid: &str) -> Vec<Vec<u8>> {
     let sudoku = Sudoku::new();
     let mut solutions: Vec<Vec<u8>> = vec![];
-    // web_sys::console::log_1(&"Before solving...".into());
-
 
     let solution = sudoku.solve(grid);
-    // web_sys::console::log_1(&"After solving...".into());
     if !solution.is_none() {
-        // web_sys::console::log_1(&"Solution not none!".into());
         let solution = solution.unwrap();
-        // web_sys::console::log_1(&"Solution unwrapped!".into());
 
         solutions.push(sudoku.format_grid(&solution).as_bytes().to_vec());
-        // web_sys::console::log_1(&"Solution converted and pushed!".into());
     }
     solutions
 }
