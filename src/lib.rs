36c1fa4fc152a5d16d9ab1da396ee1bda65fbc6f
Martin-Louis Bright <mlbright@gmail.com>
2022-01-08 13:34:08 -0500

    No modules

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@ use sudoku::Sudoku;
 
 #[wasm_bindgen]
 pub fn solve(grid: &str) -> String {
-    String::from("1234").into()
-    // let sudoku = Sudoku::new();
-    // let solution = sudoku.solve(grid);
-    // if solution.is_some() {
-    //     let solution = solution.unwrap();
-    //     sudoku.format_grid(&solution).into()
-    // } else {
-    //     String::from("").into()
-    // }
+    let sudoku = Sudoku::new();
+    let solution = sudoku.solve(grid);
+    if solution.is_some() {
+        let solution = solution.unwrap();
+        // sudoku.format_grid(&solution).into();
+        String::from("839465712146782953752391486391824675564173829287659341628537194913248567475916238").into()
+    } else {
+        // String::from("").into()
+        String::from("839465712146782953752391486391824675564173829287659341628537194913248567475916238").into()
+    }
 }
